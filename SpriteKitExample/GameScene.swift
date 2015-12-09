5e779dd0da8ba4d8dde5daaef5897e56ab6585bb
Bob Pascazio <bob@bytefly.com>
2015-12-08 19:09:39 -0500

    updates

--- SpriteKitExample/GameScene.swift
+++ SpriteKitExample/GameScene.swift
@@ -10,9 +10,15 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let player = SKSpriteNode(imageNamed: "player")
+    
     override func didMoveToView(view: SKView) {
+        
         backgroundColor = SKColor.whiteColor()
         
+        player.position = CGPoint(x: size.width*0.1, y: size.height*0.5)
+        
+        addChild(player)
         
     }
     
